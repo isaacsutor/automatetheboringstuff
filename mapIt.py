fd87f738f53a9e967af9feac16f4846d7b0d3028
isaacsutor <isaac.sutor@gmail.com>
2018-12-22 22:02:20 -0500

    mapIt now opens browser with clipboard if there is no input

--- mapIt.py
+++ mapIt.py
@@ -1,6 +1,10 @@
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
     # get address from command line
     address = ' '.join(sys.argv[1:])
 
-# TODO: Get address from clipboard
+else:
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
+
